0df9d24336e03fdf2caae83a7e8c7cb8837446e8
Henry Fung <hlf764@student.bham.ac.uk>
2019-01-23 14:22:56 +0000

    Extended GameObject
    - Changed all variables type from float to double

--- src/main/java/shared/weapons/Gun.java
+++ src/main/java/shared/weapons/Gun.java
@@ -1,41 +1,46 @@
 package shared.weapons;
 
+import shared.gameObjects.Utils.ObjectID;
+
 /**
  * @author hlf764 The abstract class for all guns type weapon.
  */
 abstract class Gun extends Weapon {
 
-  float bulletSpeed = 5.0f; // metre per second
-  float fireRate = 80.0f; // bullets per minute
-  float bulletWidth = 30.0f; // width of bullet ==(pixel?)==
-  float range = 1000.0f; // suitable for swords, explosion, laser beams
+  double bulletSpeed = 5.0;     // metre per second
+  double fireRate = 80.0;       // bullets per minute
+  double bulletWidth = 30.0;    // width of bullet ==(pixel?)==
+  double range = 1000.0;        // suitable for swords, explosion, laser beams
   boolean fullAutoFire = false; // able to shoot with full-auto or single-shot
   boolean singleHanded = false; // holding the weapon with one hand or two
 
   /**
    * Constructor of the Gun class
    *
-   * @param _damage Damage of the gun
-   * @param _weight Weight of the gun
-   * @param _name Name of the gun
-   * @param _bulletSpeed Speed of the bullets
-   * @param _fireRate Fire rate of the gun (bullets per minute)
-   * @param _bulletWidth Width of the bullet
-   * @param _range Range of the bullets they travel
-   * @param _fullAutoFire Is it full-automatic fire or single-shot
-   * @param _singleHanded Is it be hold with one hand or two hands
+   * @param _damage         Damage of the gun
+   * @param _weight         Weight of the gun
+   * @param _name           Name of the gun
+   * @param _bulletSpeed    Speed of the bullets
+   * @param _fireRate       Fire rate of the gun (bullets per minute)
+   * @param _bulletWidth    Width of the bullet
+   * @param _range          Range of the bullets they travel
+   * @param _fullAutoFire   Is it full-automatic fire or single-shot
+   * @param _singleHanded   Is it be hold with one hand or two hands
    */
   public Gun(
-      float _damage,
-      float _weight,
+      double x,
+      double y,
+      ObjectID id,
+      double _damage,
+      double _weight,
       String _name,
-      float _bulletSpeed,
-      float _fireRate,
-      float _bulletWidth,
-      float _range,
+      double _bulletSpeed,
+      double _fireRate,
+      double _bulletWidth,
+      double _range,
       boolean _fullAutoFire,
       boolean _singleHanded) {
-    super(_damage, _weight, _name);
+    super(x, y, id, _damage, _weight, _name);
     setBulletSpeed(_bulletSpeed);
     setFireRate(_fireRate);
     setBulletWidth(_bulletWidth);
@@ -48,41 +53,41 @@ abstract class Gun extends Weapon {
   // Setters and Getters
   // -------------------
 
-  public float getBulletSpeed() {
+  public double getBulletSpeed() {
     return this.bulletSpeed;
   }
 
-  public void setBulletSpeed(float newSpeed) {
+  public void setBulletSpeed(double newSpeed) {
     if (newSpeed > 0 && newSpeed < 50.0f) {
       this.bulletSpeed = newSpeed;
     }
   }
 
-  public float getFireRate() {
+  public double getFireRate() {
     return this.fireRate;
   }
 
-  public void setFireRate(float newFireRate) {
+  public void setFireRate(double newFireRate) {
     if (newFireRate > 0f) {
       this.fireRate = newFireRate;
     }
   }
 
-  public float getBulletWidth() {
+  public double getBulletWidth() {
     return this.bulletWidth;
   }
 
-  public void setBulletWidth(float newWidth) {
+  public void setBulletWidth(double newWidth) {
     if (newWidth > 0f) {
       this.bulletWidth = newWidth;
     }
   }
 
-  public float getRange() {
+  public double getRange() {
     return this.range;
   }
 
-  public void setRange(float newRange) {
+  public void setRange(double newRange) {
     if (newRange > 0) {
       this.range = newRange;
     }
--- src/main/java/shared/weapons/Handgun.java
+++ src/main/java/shared/weapons/Handgun.java
@@ -1,5 +1,7 @@
 package shared.weapons;
 
+import shared.gameObjects.Utils.ObjectID;
+
 /**
  * @author hlf764 The Handgun class.
  */
@@ -8,22 +10,33 @@ public class Handgun extends Gun {
   /**
    * Constructor of the Handgun class
    *
-   * @param _damage Damage of the gun
-   * @param _weight Weight of the gun
-   * @param _name Name of the gun
-   * @param _bulletSpeed Speed of the bullets
-   * @param _fireRate Fire rate of the gun (bullets per minute)
-   * @param _bulletWidth Width of the bullet
-   * @param _range Range of the bullets they travel
+   * @param _damage         Damage of the gun
+   * @param _weight         Weight of the gun
+   * @param _name           Name of the gun
+   * @param _bulletSpeed    Speed of the bullets
+   * @param _fireRate       Fire rate of the gun (bullets per minute)
+   * @param _bulletWidth    Width of the bullet
+   * @param _range          Range of the bullets they travel
    */
   public Handgun(
-      float _damage,
-      float _weight,
+      double x,
+      double y,
+      ObjectID id,
+      double _damage,
+      double _weight,
       String _name,
-      float _bulletSpeed,
-      float _range,
-      float _fireRate,
-      float _bulletWidth) {
-    super(_damage, _weight, _name, _bulletSpeed, _fireRate, _bulletWidth, _range, false, false);
+      double _bulletSpeed,
+      double _range,
+      double _fireRate,
+      double _bulletWidth) {
+    super(x, y, id, _damage, _weight, _name, _bulletSpeed, _fireRate, _bulletWidth, _range, false, false);
+  }
+  
+  public void update() {
+    
+  }
+  
+  public void render() {
+    
   }
 }
