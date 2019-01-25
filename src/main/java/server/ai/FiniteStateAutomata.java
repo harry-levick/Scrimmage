09aab4fe5d1897e4368d45d728906cf39fbccdf4
Harry Levick <harrylevick@hotmail.co.uk>
2019-01-25 17:12:46 +0000

    Create FSM initial state

--- src/main/java/server/ai/FiniteStateAutomata.java
+++ src/main/java/server/ai/FiniteStateAutomata.java
@@ -1,40 +1,51 @@
 package server.ai;
 
+/** @author Harry Levick (hxl799) */
 public enum FiniteStateAutomata {
 
   ATTACKING() {
     public FiniteStateAutomata next() {
-
+      return null;
     }
 
   },
   CHASING() {
     public FiniteStateAutomata next() {
-
+      return null;
     }
   },
   CHASING_ATTACKING() {
     public FiniteStateAutomata next() {
-
+      return null;
     }
   },
   FLEEING() {
     public FiniteStateAutomata next() {
-
+      return null;
     }
   },
   FLEEING_ATTACKING() {
     public FiniteStateAutomata next() {
-
+      return null;
     }
   },
   STILL() {
     public FiniteStateAutomata next() {
-
+      return null;
+    }
+  },
+  INITIAL_STATE() {
+    // The initial state just acts as an entry point, and so directs straight to the still state.
+    public FiniteStateAutomata next() {
+      return FiniteStateAutomata.STILL;
     }
   };
 
   public abstract FiniteStateAutomata next();
+
+  FiniteStateAutomata() {
+
+  }
 
 
 }
