2e06383734f8e4ad63da8ff9e9cdcfb6c8495e96
Harry Levick <harrylevick@hotmail.co.uk>
2019-01-26 18:54:04 +0000

    implement a target player for each bot
    the bot now finds the closest player to itself and locks onto this player, so it will only focus on one player at a time (based on distance).

--- src/main/java/server/ai/AiAgent.java
+++ src/main/java/server/ai/AiAgent.java
@@ -1,18 +1,26 @@
 package server.ai;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import shared.gameObjects.GameObject;
 import shared.gameObjects.Utils.ObjectID;
+import shared.gameObjects.players.Player;
 
 /** @author Harry Levick (hxl799) */
 public class AiAgent {
 
-  Bot player;
+  Bot bot;
   FiniteStateAutomata state;
   boolean active;
+  ArrayList<GameObject> gameObjects;
+  Player targetPlayer;
 
-  public AiAgent(double xPos, double yPos, ObjectID id) {
-    player = new Bot(xPos, yPos, id);
-    state = FiniteStateAutomata.INITIAL_STATE;
-    active = false;
+  public AiAgent(double xPos, double yPos, ObjectID id, ArrayList<GameObject> gameObjects) {
+    this.bot = new Bot(xPos, yPos, id);
+    this.state = FiniteStateAutomata.INITIAL_STATE;
+    this.active = false;
+    this.gameObjects = gameObjects;
   }
 
   /**
@@ -20,10 +28,19 @@ public class AiAgent {
    */
   public void startAgent() {
     active = true;
-    
-    while (active) {
+    /**
+     * Would I need to fetch all players on each loop, or would a single fetch outside of the loop
+     * give a reference to both players that updates with the player updates?
+     */
+    // Collect all players from the world
+    List<Player> allPlayers = gameObjects.stream()
+        .filter(p -> p instanceof Player)
+        .collect(Collectors.toList());
 
-      state = state.next();
+    targetPlayer = findTarget(allPlayers);
+
+    while (active) {
+      state = state.next(targetPlayer, bot);
 
       /**
        * The ai can be in one of 6 states at any one time.
@@ -31,7 +48,6 @@ public class AiAgent {
        */
       switch (state) {
         case STILL:
-          break;
 
         case CHASING:
 
@@ -44,8 +60,41 @@ public class AiAgent {
         case FLEEING_ATTACKING:
 
         default:
+          // Update the targeted player
+          targetPlayer = findTarget(allPlayers);
       }
     }
 
+  }
+
+  /**
+   * Finds the closest player
+   * @param allPlayers A list of all players in the world
+   * @return The player who is the closest to the bot
+   */
+  private Player findTarget(List<Player> allPlayers) {
+    Player target = null;
+    double targetX, targetY, changeX, changeY;
+    double botX = bot.getX();
+    double botY = bot.getY();
+    double targetDistance = Double.POSITIVE_INFINITY;
+
+    for (Player p : allPlayers) {
+      targetX = p.getX();
+      targetY = p.getY();
+
+      changeX = targetX - botX;
+      changeY = targetY - botY;
+
+      double distance = Math.sqrt(Math.pow(changeX, 2) + Math.pow(changeY, 2));
+      // Update the target if another player is closer
+      if (distance < targetDistance) {
+        targetDistance = distance;
+        target = p;
+      }
+
+    }
+    
+    return target;
   }
 }
--- src/main/java/server/ai/FiniteStateAutomata.java
+++ src/main/java/server/ai/FiniteStateAutomata.java
@@ -1,11 +1,13 @@
 package server.ai;
 
+import shared.gameObjects.GameObject;
+
 /** @author Harry Levick (hxl799) */
 public enum FiniteStateAutomata {
 
 
   ATTACKING() {
-    public FiniteStateAutomata next() {
+    public FiniteStateAutomata next(GameObject targetPlayer, Bot bot) {
       int enemyDistance;
       int weaponRange;
       int botHealth;
@@ -30,38 +32,38 @@ public enum FiniteStateAutomata {
 
   },
   CHASING() {
-    public FiniteStateAutomata next() {
+    public FiniteStateAutomata next(GameObject targetPlayer, Bot bot) {
       return null;
     }
   },
   CHASING_ATTACKING() {
-    public FiniteStateAutomata next() {
+    public FiniteStateAutomata next(GameObject targetPlayer, Bot bot) {
       return null;
     }
   },
   FLEEING() {
-    public FiniteStateAutomata next() {
+    public FiniteStateAutomata next(GameObject targetPlayer, Bot bot) {
       return null;
     }
   },
   FLEEING_ATTACKING() {
-    public FiniteStateAutomata next() {
+    public FiniteStateAutomata next(GameObject targetPlayer, Bot bot) {
       return null;
     }
   },
   STILL() {
-    public FiniteStateAutomata next() {
+    public FiniteStateAutomata next(GameObject targetPlayer, Bot bot) {
       return null;
     }
   },
   INITIAL_STATE() {
     // The initial state just acts as an entry point, and so directs straight to the still state.
-    public FiniteStateAutomata next() {
+    public FiniteStateAutomata next(GameObject targetPlayer, Bot bot) {
       return FiniteStateAutomata.STILL;
     }
   };
 
-  public abstract FiniteStateAutomata next();
+  public abstract FiniteStateAutomata next(GameObject targetPlayer, Bot bot);
   final int highHealth = 66;
   final int mediumHealth = 33;
 
