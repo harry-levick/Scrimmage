14deef0752ac28dc4c0adf677a9498fd0704d1e0
Harry Levick <harrylevick@hotmail.co.uk>
2019-01-27 14:39:57 +0000

    complete chasing state transitions

--- src/main/java/server/ai/FSA.java
+++ src/main/java/server/ai/FSA.java
@@ -8,14 +8,13 @@ public enum FSA {
 
 
   ATTACKING() {
-    public FSA next(Player targetPlayer, Bot bot, double prevDist, double newDist) {
-      Melee tmp;
-      double weaponRange = (bot.getHolding().isGun()) ? Double.POSITIVE_INFINITY :
-          (tmp = (Melee) bot.getHolding()).getRange();
-      int ammoLeft = (bot.getHolding().isGun()) ? bot.getHolding().getAmmo() : 0;
+    public FSA next(Player targetPlayer, Player bot, double prevDist, double newDist) {
+      StateInfo.setInfo(targetPlayer, bot);
 
-      int botHealth = bot.getHealth();
-      int enemyHealth = targetPlayer.getHealth();
+      double weaponRange = StateInfo.weaponRange;
+      int ammoLeft = StateInfo.ammoLeft;
+      int botHealth = StateInfo.botHealth;
+      int enemyHealth = StateInfo.enemyHealth;
 
       if ((newDist > weaponRange) &&
           ((botHealth >= this.HIGH_HEALTH) || (botHealth >= enemyHealth * 1.5)) ) {
@@ -41,38 +40,75 @@ public enum FSA {
 
   },
   CHASING() {
-    public FSA next(Player targetPlayer, Bot bot, double prevDist, double newDist) {
-      return null;
+    public FSA next(Player targetPlayer, Player bot, double prevDist, double newDist) {
+      StateInfo.setInfo(targetPlayer, bot);
+
+      double weaponRange = StateInfo.weaponRange;
+      int ammoLeft = StateInfo.ammoLeft;
+      int botHealth = StateInfo.botHealth;
+      int enemyHealth = StateInfo.enemyHealth;
+
+      if ((newDist <= weaponRange) &&
+          (ammoLeft >= 0) &&
+          (botHealth >= this.MEDIUM_HEALTH) &&
+          // Target staying relatively still
+          (prevDist * 1.05 <= newDist && newDist <= prevDist * 1.05 )) {
+        return ATTACKING;
+
+      } else if ((newDist <= weaponRange) &&
+          (newDist > prevDist) &&
+          (botHealth >= this.HIGH_HEALTH) || (botHealth >= enemyHealth * 1.5)) {
+        return CHASING_ATTACKING;
+
+      } else if ((botHealth <= this.MEDIUM_HEALTH) ||
+          (bot.getHolding().isGun()) && (ammoLeft == 0)) {
+        return FLEEING;
+
+      } else if ((botHealth <= this.HIGH_HEALTH) &&
+          (botHealth >= this.MEDIUM_HEALTH) &&
+          (newDist <= prevDist) &&
+          (newDist <= weaponRange)) {
+        return FLEEING_ATTACKING;
+
+      } else return CHASING;
+
     }
   },
   CHASING_ATTACKING() {
-    public FSA next(Player targetPlayer, Bot bot, double prevDist, double newDist) {
+    public FSA next(Player targetPlayer, Player bot, double prevDist, double newDist) {
       return null;
     }
   },
   FLEEING() {
-    public FSA next(Player targetPlayer, Bot bot, double prevDist, double newDist) {
+    public FSA next(Player targetPlayer, Player bot, double prevDist, double newDist) {
       return null;
     }
   },
   FLEEING_ATTACKING() {
-    public FSA next(Player targetPlayer, Bot bot, double prevDist, double newDist) {
+    public FSA next(Player targetPlayer, Player bot, double prevDist, double newDist) {
       return null;
     }
   },
   STILL() {
-    public FSA next(Player targetPlayer, Bot bot, double prevDist, double newDist) {
+    public FSA next(Player targetPlayer, Player bot, double prevDist, double newDist) {
       return null;
     }
   },
   INITIAL_STATE() {
     // The initial state just acts as an entry point, and so directs straight to the still state.
-    public FSA next(Player targetPlayer, Bot bot, double prevDist, double newDist) {
+    public FSA next(Player targetPlayer, Player bot, double prevDist, double newDist) {
       return FSA.STILL;
     }
   };
 
-  public abstract FSA next(Player targetPlayer, Bot bot, double prevDist, double newDist);
+  public abstract FSA next(Player targetPlayer, Player bot, double prevDist, double newDist);
+
+  private Object[] retrieveInfo(Player target, Player bot) {
+    Object[] info = new Object[5];
+
+    return info;
+  }
+
   final int HIGH_HEALTH = 66;
   final int MEDIUM_HEALTH = 33;
 
