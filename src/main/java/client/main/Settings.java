1f04606564fe054ea7253e2b2db3b4c991b0a135
llc721 <llc721@student.bham.ac.uk>
2019-01-28 10:26:40 +0000

    Updated path to relative path

--- src/main/java/client/main/Settings.java
+++ src/main/java/client/main/Settings.java
@@ -8,9 +8,9 @@ public class Settings {
   private int musicVolume;
   private int soundEffectVolume;
   private String mapsPath =
-      "C:\\Users\\brett\\projects\\aloneinthedark\\src\\main\\resources\\menus";
+      "src\\main\\resources\\menus";
   private String menuPath =
-      "C:\\Users\\brett\\projects\\aloneinthedark\\src\\main\\resources\\menus";
+      "src\\main\\resources\\menus";
 
   /**
    * Default Constructor Music volume set to 100 and sound effects to 75
