b17c039301551ecca800815a5bc64d89c4fad9c5
Faisal IMH <fxa579@cs.bham.ac.uk>
2019-01-29 20:48:03 +0000

    Adjusted Force and Rigibody for update method, Added temporary timestep to static Physics class

--- src/main/java/shared/gameObjects/Components/Rigidbody.java
+++ src/main/java/shared/gameObjects/Components/Rigidbody.java
@@ -1,9 +1,12 @@
 package shared.gameObjects.components;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import shared.gameObjects.GameObject;
 import shared.gameObjects.Utils.Transform;
+import shared.physics.Physics;
 import shared.physics.data.AngularData;
+import shared.physics.data.Collision;
 import shared.physics.data.Force;
 import shared.physics.data.MaterialProperty;
 import shared.physics.types.RigibodyType;
@@ -16,49 +19,69 @@ import shared.util.maths.Vector2;
  */
 public class Rigidbody extends Component implements Serializable {
 
+  private Vector2 deltaPos;
   private Vector2 velocity;
-  private Force currentForce;
+  private Vector2 currentForce;
+  private Vector2 lastAcceleration;
+  private Vector2 acceleration;
 
   private RigibodyType bodyType;
   private RigidbodyUpdateType updateMethod;
   private MaterialProperty material;
   private AngularData angularData;
+  private ArrayList<Collision> collisions;
 
   private float mass;
   private float gravityScale;
+  private float airDrag;
+
+  private boolean grounded;
 
   public Rigidbody(
       RigibodyType bodyType,
       RigidbodyUpdateType updateMethod,
       float mass,
       float gravityScale,
+      float airDrag,
       MaterialProperty material,
       AngularData angularData,
       GameObject parent) {
     super(parent, ComponentType.RIGIBODY);
     this.gravityScale = gravityScale;
     this.mass = mass;
+    this.airDrag = airDrag;
     this.material = material;
     this.angularData = angularData;
     this.bodyType = bodyType;
     this.updateMethod = updateMethod;
 
-    velocity = Vector2.Zero();
-    currentForce = new Force();
+    collisions = new ArrayList<>();
+    velocity = acceleration = lastAcceleration = deltaPos = Vector2.Zero();
+    currentForce = acceleration.mult(mass);
   }
 
   // Update Methods
   public void update() {
     // TODO Add Physics Updates Here
+    //Apply Collision Forces
+    //Update Forces
+    //Update Velocity
+    //Move Body
+    lastAcceleration = acceleration;
+    deltaPos = deltaPos.add(velocity.mult(Physics.TIMESTEP).add(acceleration.mult(0.5f).mult(Physics.TIMESTEP*Physics.TIMESTEP )));
+    acceleration = currentForce.div(mass);
+    acceleration = lastAcceleration.add(acceleration).div(2);
+    velocity = velocity.add(acceleration.mult(Physics.TIMESTEP));
   }
   // Force Methods
 
   /** Applies a force of a defined from a defined source direction (Instantaneous Force) */
-  void addForce(float force, Transform source) {}
+  void addForce(Vector2 force, Transform source) {}
 
   /** Moves the Rigibody to the defined space over time, instant if 0 */
   void move(Vector2 position, float time) {}
 
+  //Update Methods
   // Getters and Setters
   public Vector2 getVelocity() {
     return velocity;
--- src/main/java/shared/physics/Physics.java
+++ src/main/java/shared/physics/Physics.java
@@ -3,7 +3,8 @@ package shared.physics;
 /** @author fxa579 The singleton class respomsible for raycasting and physics constants/equations */
 public class Physics {
 
-  public static final double GRAVITY = -9.81;
+  public static final float GRAVITY = -9.81f;
+  public static final float TIMESTEP = 1f/60;
   private static Physics ourInstance = new Physics();
 
   private Physics() {}
