cd95d47f0f180547757fa8ae5e1bc9b9aaa5dd71
Brett Saunders <bjs730@cs.bham.ac.uk>
2019-02-03 23:57:26 +0000

    Fixed most JUnit Tests
    Signed-off-by: Brett Saunders <bjs730@cs.bham.ac.uk>

--- src/test/java/shared/packets/PacketTest.java
+++ src/test/java/shared/packets/PacketTest.java
@@ -3,9 +3,10 @@ package shared.packets;
 
 import static junit.framework.TestCase.assertEquals;
 
+import java.util.UUID;
 import org.junit.Test;
 
-public class PacketInputTest {
+public class PacketTest {
 
   private double x = 10;
   private double y = 213.12;
@@ -13,9 +14,11 @@ public class PacketInputTest {
   private boolean rightKey = true;
   private boolean jumpKey = true;
   private boolean click = false;
+  private String username = "TheBigMJ";
+  private UUID uuid = UUID.randomUUID();
 
   @Test
-  public void sameInSameOut() {
+  public void InputPacketTest() {
     PacketInput input = new PacketInput(x, y, leftKey, rightKey, jumpKey, click);
     byte[] simulateNetwork = input.getData();
     PacketInput output = new PacketInput(simulateNetwork);
@@ -25,6 +28,15 @@ public class PacketInputTest {
     assertEquals(output.isRightKey(), rightKey);
     assertEquals(output.isJumpKey(), jumpKey);
     assertEquals(output.isClick(), click);
+  }
+
+  @Test
+  public void JoinPacketTest() {
+    PacketJoin join = new PacketJoin(uuid, username);
+    byte[] simulateNetwork = join.getData();
+    PacketJoin output = new PacketJoin(simulateNetwork);
+    assertEquals(output.getClientID(), uuid);
+    assertEquals(output.getUsername(), username);
   }
 
 }
