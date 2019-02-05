c63b7d0527276ea164f062e42ce8da58f080d0a9
Brett James Saunders <bjs730@cs.bham.ac.uk>
2019-02-05 11:09:11 +0000

    Added Path parser
    Signed-off-by: Brett James Saunders <bjs730@cs.bham.ac.uk>

---
+++ src/main/java/shared/util/Path.java
@@ -0,0 +1,16 @@
+package shared.util;
+
+import java.io.File;
+
+public class Path {
+
+  public static String parse(String... args) {
+    String temp = "";
+    for (int i = 0; i < args.length - 1; i++) {
+      temp += args[i] + File.separator;
+    }
+    temp += args[args.length - 1];
+    return temp;
+  }
+
+}
