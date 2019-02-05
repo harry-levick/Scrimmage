48bc05d428982b337ce5966130021037d2ec97b7
Brett Saunders <bjs730@cs.bham.ac.uk>
2019-02-05 23:10:05 +0000

    Added logo and Packets
    Signed-off-by: Brett Saunders <bjs730@cs.bham.ac.uk>

--- src/main/java/shared/packets/PacketEnd.java
+++ src/main/java/shared/packets/PacketEnd.java
@@ -2,4 +2,14 @@ package shared.packets;
 
 public class PacketEnd extends Packet {
 
+  public PacketEnd() {
+    packetID = PacketID.END.getID();
+    data = (Integer.toString(packetID)).getBytes();
+  }
+
+  public PacketEnd(String data) {
+    String[] unpackedData = data.split(",");
+    this.packetID = Integer.parseInt(unpackedData[0]);
+  }
+
 }
--- src/main/java/shared/packets/PacketPlayerJoin.java
+++ src/main/java/shared/packets/PacketPlayerJoin.java
@@ -1,5 +1,38 @@
 package shared.packets;
 
+import java.util.UUID;
+
 public class PacketPlayerJoin extends Packet {
 
+  private double x;
+  private double y;
+  private UUID uuid;
+
+  public PacketPlayerJoin(double x, double y, UUID uuid) {
+    packetID = PacketID.PLAYERJOIN.getID();
+    this.x = x;
+    this.y = y;
+    this.uuid = uuid;
+    data = (packetID + x + y + uuid.toString()).getBytes();
+  }
+
+  public PacketPlayerJoin(String data) {
+    String[] unpackedData = data.split(",");
+    this.packetID = Integer.parseInt(unpackedData[0]);
+    this.x = Double.parseDouble(unpackedData[1]);
+    this.y = Double.parseDouble(unpackedData[2]);
+    this.uuid = UUID.fromString(unpackedData[3]);
+  }
+
+  public double getX() {
+    return x;
+  }
+
+  public double getY() {
+    return y;
+  }
+
+  public UUID getUUID() {
+    return uuid;
+  }
 }
