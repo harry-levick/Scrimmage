3767a33d944a13af68e16c7f60649e68f06cad0d
Henry Fung <hlf764@cs.bham.ac.uk>
2019-02-17 18:08:26 +0000

    Added rotation to rendering and addComponent(this)

--- src/main/java/shared/gameObjects/weapons/Melee.java
+++ src/main/java/shared/gameObjects/weapons/Melee.java
@@ -3,13 +3,21 @@ package shared.gameObjects.weapons;
 import java.util.UUID;
 import shared.gameObjects.Utils.ObjectID;
 import shared.gameObjects.components.CircleCollider;
+import shared.gameObjects.components.Rigidbody;
 import shared.physics.Physics;
+import shared.physics.data.AngularData;
+import shared.physics.data.MaterialProperty;
+import shared.physics.types.RigidbodyType;
 
 public abstract class Melee extends Weapon {
 
   protected double range; // radius in pixels
   protected double beginAngle; // swing from beginAngle (relative to arm)
   protected double endAngle; // swing till endAngle (relative to arm)
+  protected Rigidbody rb;
+  protected boolean attacking;
+  protected double[] angles;
+  protected int currentAngleIndex;
 
   public Melee(
       double x,
@@ -31,6 +39,21 @@ public abstract class Melee extends Weapon {
     this.range = range;
     this.beginAngle = beginAngle;
     this.endAngle = endAngle;
+    this.angles = generateAngles();
+    this.attacking = false;
+    this.currentAngleIndex = 0;
+    System.out.println("@Melee angles[0]="+this.angles[0]);
+    
+    addComponent(new CircleCollider(this, (float)range, false));
+    rb = new Rigidbody(
+        RigidbodyType.STATIC,
+        50f,
+        0,
+        0.1f,
+        new MaterialProperty(0, 1, 1),
+        new AngularData(0, 0, 0, 0),
+        this);//TODO FIX
+    addComponent(rb);
   }
 
   @Override
@@ -42,6 +65,7 @@ public abstract class Melee extends Weapon {
   @Override
   public void fire(double mouseX, double mouseY) {
     if (canFire()) {
+      this.attacking = true;
       UUID uuid = UUID.randomUUID();
       // TODO: add circle cast
       // maybe use box cast?
@@ -50,10 +74,27 @@ public abstract class Melee extends Weapon {
       deductAmmo();
     }
   }
+  
+  private double[] generateAngles() {
+    double[] angle = new double[(int) (beginAngle + endAngle) + 1];
+    int k = 0;
+    for (double i = beginAngle; i >= (endAngle * -1); i--) {
+      angle[k] = i;
+      ++k;
+    }
+    
+    return angle;
+  }
 
   // -------------------
   // Setters and Getters
   // -------------------
+  public double getAngle(int index) {
+    if (index < (int)(beginAngle + endAngle + 1))
+      return angles[index];
+    return 0;
+  }
+  
   public double getRange() {
     return this.range;
   }
