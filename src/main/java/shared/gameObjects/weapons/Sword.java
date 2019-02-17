cb2ec803cfeeabd999b248b8516f2c29590ba365
Henry Fung <hlf764@cs.bham.ac.uk>
2019-02-17 18:07:49 +0000

    Changed constructor parameters, added rotation to sword rendering

--- src/main/java/shared/gameObjects/weapons/Sword.java
+++ src/main/java/shared/gameObjects/weapons/Sword.java
@@ -5,7 +5,11 @@ import shared.gameObjects.Utils.ObjectID;
 
 public class Sword extends Melee {
 
-  private static String imagePath = "images/weapons/sword.jpg";
+  //private static String imagePath = "images/weapons/sword.jpg";
+  private static String imagePath = "images/weapons/sword3.png";
+  private double rotate;
+  private double[] angles;
+  private int currentAngleIndex;
 
   /**
    * Constructor of the Sword class
@@ -18,18 +22,27 @@ public class Sword extends Melee {
    * @param endAngle The ending angle when the sword swing
    * @param uuid The UUID of the sword
    */
-  public Sword(double x, double y, double sizeX, double sizeY, String name, double range,
-      double beginAngle, double endAngle, UUID uuid) {
-    super(x, y, sizeX, sizeY, ObjectID.Weapon, 20, 10, name, 30,
-        60, range, beginAngle, endAngle, uuid);
+  public Sword(double x, double y, double sizeX, double sizeY, String name, UUID uuid) {
+    super(
+        x,
+        y,
+        sizeX, 
+        sizeY, 
+        ObjectID.Weapon,
+        20,  // damage
+        10,  // weight
+        name, 
+        30,  // ammo
+        60,  // fireRate
+        30,  // range
+        50,  // beginAngle
+        20,  // endAngle
+        uuid);
   }
-
+  
   @Override
   public void fire(double mouseX, double mouseY) {
-    if (canFire()) {
-      //swing
-      this.currentCooldown = getDefaultCoolDown();
-    }
+    super.fire(mouseX, mouseY);
   }
   
   @Override
@@ -42,6 +55,18 @@ public class Sword extends Melee {
     super.render();
     imageView.setTranslateX(this.getX());
     imageView.setTranslateY(this.getY());
+    
+    // set rotation of the sword
+    if (this.attacking) {
+      this.imageView.setRotate(45 + (-1 * getAngle(currentAngleIndex)));
+      // set incrementation of angles for frames
+      currentAngleIndex += 4;
+      if (currentAngleIndex >= (int)(beginAngle + endAngle + 1)) {
+        attacking = false;
+        currentAngleIndex = 0;
+        this.imageView.setRotate(0);
+      }
+    }
   }
 
   @Override
