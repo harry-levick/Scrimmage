c1eabeaccdbcb74702b2c1d974256aa86047a4d3
Henry Fung <hlf764@cs.bham.ac.uk>
2019-02-19 18:27:49 +0000

    Added methods to get hand positions

--- src/main/java/shared/gameObjects/players/Player.java
+++ src/main/java/shared/gameObjects/players/Player.java
@@ -27,6 +27,8 @@ public class Player extends GameObject {
   protected float jumpTime;
   protected boolean jumped;
   protected boolean grounded;
+  protected boolean facingLeft;
+  protected boolean facingRight;
   protected int health;
   protected Weapon holding;
   protected Rigidbody rb;
@@ -122,11 +124,15 @@ public class Player extends GameObject {
       rb.moveX(speed);
       animation.switchAnimation("walk");
       imageView.setScaleX(1);
+      this.facingLeft = false;
+      this.facingRight = true;
     }
     if (leftKey) {
       rb.moveX(speed * -1);
       animation.switchAnimation("walk");
       imageView.setScaleX(-1);
+      this.facingRight = false;
+      this.facingLeft = true;
     }
 
     if (!rightKey && !leftKey) {
@@ -168,7 +174,7 @@ public class Player extends GameObject {
       connectionHandler.send(input.getData());
     }
   }
-
+  
   /**
    * Check if the current holding weapon is valid or not
    *
@@ -242,5 +248,89 @@ public class Player extends GameObject {
 
   public int getScore() {
     return score;
+  }
+  
+  /**
+   * Hand position x of the player when facing left
+   * 
+   * @return x position of the hand
+   */
+  public double getHandLeftX() {
+    return this.getX() + 13;
+  }
+  
+  /**
+   * Hand position y of the player when facing left
+   * 
+   * @return y position of the hand
+   */
+  public double getHandLeftY() {
+    return this.getY() + 90;
+  }
+  
+  /**
+   * Hand position x of the player when facing right
+   * 
+   * @return x position of the hand
+   */
+  public double getHandRightX() {
+    return this.getX() + 60;
+  }
+  
+  /**
+   * Hand position y of the player when facing right 
+   * 
+   * @return y position of the hand
+   */
+  public double getHandRightY() {
+    return this.getY() + 90;
+  }
+  
+  /**
+   * Hand position x of the player when jumping and facing left
+   * 
+   * @return x position of the hand
+   */
+  public double getHandLeftJumpX() {
+    return this.getX() + 7;
+  }
+  
+  /**
+   * Hand position y of the player when jumping and facing left
+   * 
+   * @return y position of the hand
+   */
+  public double getHandLeftJumpY() {
+    return this.getY() + 30;
+  }
+  
+  /**
+   * Hand position x of the player when jumping and facing right
+   * 
+   * @return x position of the hand
+   */
+  public double getHandRightJumpX() {
+    return this.getX() + 67;
+  }
+  
+  /**
+   * Hand position y of the player when jumping and facing right
+   * 
+   * @return y position of the hand
+   */
+  public double getHandRightJumpY() {
+    return this.getY() + 30;
+  }
+  
+  public boolean getJumped() {
+    return this.jumped;
+  }
+  
+  public boolean getFacingLeft() {
+    return this.facingLeft;
+  }
+  
+  public boolean getFacingRight() {
+    return this.facingRight;
   }
 }
