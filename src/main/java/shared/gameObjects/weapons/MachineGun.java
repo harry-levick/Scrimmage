143223a48c7cb6e4106a8a0ad8a2d8c5eb8ed09d
Henry Fung <hlf764@student.bham.ac.uk>
2019-02-21 13:48:07 +0000

    Render works on facing right (hand=grip)

--- src/main/java/shared/gameObjects/weapons/MachineGun.java
+++ src/main/java/shared/gameObjects/weapons/MachineGun.java
@@ -8,6 +8,7 @@ import shared.util.Path;
 public class MachineGun extends Gun {
 
   private static String imagePath = "images/weapons/machinegun.png"; // path to Machine Gun image
+  private double[] holderHandPos;
 
   public MachineGun(double x, double y, String name, Player holder,
       UUID uuid) {
@@ -15,8 +16,8 @@ public class MachineGun extends Gun {
     super(
         x,
         y,
-        116,
-        33,
+        80,
+        20,
         ObjectID.Weapon, // ObjectID
         5, // damage
         10, // weight
@@ -29,18 +30,21 @@ public class MachineGun extends Gun {
         true, // fullAutoFire
         false, // singleHanded
         uuid);
+    holderHandPos = holder.getHandPos();
   }
 
   @Override
   public void fire(double mouseX, double mouseY) {
     if (canFire()) {
       UUID uuid = UUID.randomUUID();
+      double bulletX = holderHandPos[0] + (holder.getFacingRight()? getMuzzleX() : getMuzzleFlipX());
+      double bulletY = holderHandPos[1] + (holder.getFacingRight()? getMuzzleY() : getMuzzleFlipY()); 
       Bullet bullet =
           new MachineGunBullet(
-              getX() + 106,
-              getY(),
-              20,
-              20,
+              //getX() + (holder.getFacingRight()? getMuzzleX() : getMuzzleFlipX()),
+              //getY() + (holder.getFacingRight()? getMuzzleY() : getMuzzleFlipY()),
+              bulletX,
+              bulletY,
               mouseX,
               mouseY,
               this.bulletWidth,
@@ -56,13 +60,33 @@ public class MachineGun extends Gun {
   @Override
   public void update() {
     super.update();
+    holderHandPos = holder.getHandPos();
   }
 
   @Override
   public void render() {
     super.render();
+    /*
     imageView.setTranslateX(this.getX());
     imageView.setTranslateY(this.getY());
+    */
+    /*
+    if (holder.getFacingLeft()) {
+      imageView.setScaleX(-1);
+      imageView.setTranslateX(holderHandPos[0] + this.getGripFlipX());
+      imageView.setTranslateY(holderHandPos[1] + this.getGripFlipY());
+    }
+    else if (holder.getFacingRight()) {
+      imageView.setScaleX(1);
+      imageView.setTranslateX(holderHandPos[0] + this.getGripX());
+      imageView.setTranslateY(holderHandPos[1] + this.getGripY());
+    }
+    */
+    
+    System.out.println(String.format("(%fx,%fy) (%fgx,%fgy) (%fhx,%fhy)", this.getX(), this.getY(), this.getGripX(), this.getGripY(), this.holderHandPos[0], this.holderHandPos[1]));
+    imageView.setTranslateX(this.getGripX());
+    imageView.setTranslateY(this.getGripY());
+    
   }
 
   @Override
@@ -76,7 +100,38 @@ public class MachineGun extends Gun {
 
   @Override
   public void initialiseAnimation() {
-    // this.animation.supplyAnimation("default", imagePath);
     this.animation.supplyAnimationWithSize("default", 40, 40, true, Path.convert(this.imagePath));
+  }
+  
+  public double getGripX() {
+    return holderHandPos[0] - 20;
+  }
+  
+  public double getGripY() {
+    return holderHandPos[1] - 8;
+  }
+  
+  public double getGripFlipX() {
+    return -20;
+  }
+  
+  public double getGripFlipY() {
+    return -10;
+  }
+  
+  public double getMuzzleX() {
+    return 54;
+  }
+  
+  public double getMuzzleY() {
+    return 6;
+  }
+  
+  public double getMuzzleFlipX() {
+    return -80;
+  }
+  
+  public double getMuzzleFlipY() {
+    return -10;
   }
 }
