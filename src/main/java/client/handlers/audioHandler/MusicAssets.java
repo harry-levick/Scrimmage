41c08bd9c141ac0aa3e1a9007ac8400fbf1ebda9
llc721 <llc721@student.bham.ac.uk>
2019-02-22 01:06:59 +0000

    Removed old test tracks

--- src/main/java/client/handlers/audioHandler/MusicAssets.java
+++ src/main/java/client/handlers/audioHandler/MusicAssets.java
@@ -28,29 +28,6 @@ public class MusicAssets {
     tracks.put("MAN_DOWN", "man-down-by-kevin-macleod.mp3");
     tracks.put("OBLITERATION", "obliteration-by-kevin-macleod.mp3");
     tracks.put("SATIATE", "satiate---only-percussion-by-kevin-macleod.mp3");
-    tracks.put("1", "1.mp3");
-    tracks.put("2", "2.mp3");
-    tracks.put("3", "3.mp3");
-    tracks.put("4", "4.mp3");
-    tracks.put("5", "5.mp3");
-    tracks.put("6", "6.mp3");
-    tracks.put("7", "7.mp3");
-    tracks.put("8", "8.mp3");
-    tracks.put("9", "9.mp3");
-    tracks.put("10", "10.mp3");
-    playlist.add("1");
-    playlist.add("2");
-    playlist.add("3");
-    playlist.add("4");
-    playlist.add("5");
-    playlist.add("6");
-    playlist.add("7");
-    playlist.add("8");
-    playlist.add("9");
-    playlist.add("10");
-
-
-
   }
 
   protected String getTrackPath(String trackIndex) {
