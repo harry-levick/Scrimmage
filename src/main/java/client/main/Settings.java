5eb62802af0929130dd40fc47ba1caa30363ca50
llc721 <llc721@student.bham.ac.uk>
2019-03-05 23:16:13 +0000

    Readability refactor

--- src/main/java/client/main/Settings.java
+++ src/main/java/client/main/Settings.java
@@ -1,6 +1,7 @@
 package client.main;
 
 import java.io.File;
+import javafx.stage.Screen;
 import shared.handlers.levelHandler.LevelHandler;
 
 /**
@@ -10,24 +11,36 @@ public class Settings {
 
   public static LevelHandler levelHandler;
 
-  private String username = "TestAccount";
-  private int port = 4446;
-  private double musicVolume = 0;
-  private double soundEffectVolume = 0.75;
-  private String mapsPath =
-      "src" + File.separator + "main" + File.separator + "resources" + File.separator + "menus";
-  private String menuPath =
-      "src" + File.separator + "main" + File.separator + "resources" + File.separator + "menus";
-  private String musicPath =
-      "src" + File.separator + "main" + File.separator + "resources" + File.separator + "audio"
-          + File.separator + "music";
-  private String SFXPath =
-      "src" + File.separator + "main" + File.separator + "resources" + File.separator + "audio"
-          + File.separator + "sound-effects";
+  private String username;
+  private int port;
+  private double musicVolume;
+  private double soundEffectVolume;
+  private String mapsPath;
+  private String menuPath;
+  private String musicPath;
+  private String SFXPath;
+  private int windowWidth = (int) Screen.getPrimary().getBounds().getWidth();
+  private int windowHeight = (int) Screen.getPrimary().getBounds().getHeight();
+  private int mapWidth;
+  private int mapHeight;
+  private String s = File.separator;
+
   /**
    * Default Constructor Music volume set to 100 and sound effects to 75
    */
   public Settings() {
+    //settings that are set arbitrarily
+    username = "TestAccount";
+    port = 4446;
+    musicVolume = 0;
+    soundEffectVolume = 0;
+    mapWidth = 1920;
+    mapHeight = 1080;
+
+    mapsPath = "src" + s + "main" + s + "resources" + s + "maps";
+    menuPath = "src" + s + "main" + s + "resources" + s + "menus";
+    musicPath = "src" + s + "main" + s + "resources" + s + "audio" + s + "music";
+    SFXPath = "src" + s + "main" + s + "resources" + s + "audio" + s + "sound-effects";
   }
 
   public String getMapsPath() {
@@ -44,6 +57,14 @@ public class Settings {
 
   public void setMenuPath(String menuPath) {
     this.menuPath = menuPath;
+  }
+
+  public int getWindowWidth() {
+    return windowWidth;
+  }
+
+  public int getWindowHeight() {
+    return windowHeight;
   }
 
   public String getMusicPath() {
