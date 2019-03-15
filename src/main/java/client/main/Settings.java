335d5bcd2b76580412ca75d0a135177d21488000
llc721 <llc721@student.bham.ac.uk>
2019-03-15 17:06:16 +0000

    Javadoc - Settings

--- src/main/java/client/main/Settings.java
+++ src/main/java/client/main/Settings.java
@@ -9,7 +9,7 @@ import javafx.stage.Screen;
 import shared.handlers.levelHandler.LevelHandler;
 
 /**
- * @author Brett Saunders
+ * Global settings config of the game. Defines game settings and resource paths.
  */
 public class Settings {
 
@@ -59,62 +59,125 @@ public class Settings {
     fontPath = resourcesPath + s + "Kenney Future.ttf";
   }
 
+  /**
+   * The path without an ending file separator to the resources
+   *
+   * @return The path containing File.Separator
+   */
   public String getResourcesPath() {
     return resourcesPath;
   }
 
+  /**
+   * Scales a grid position to its pixel position (top left)
+   * @param gridPos The grid square count
+   * @return Scales grid squares to the pixels
+   */
   public int getGrisPos(int gridPos) {
     return gridPos * gridSize;
   }
 
+  /**
+   * The path of the maps without an ending file separator to the resources
+   * @return The relative path containing File.Separator
+   */
   public String getMapsPath() {
     return mapsPath;
   }
 
+  /**
+   * Sets the full relative path of the maps directory
+   * @param mapsPath The new directory of the maps
+   */
   public void setMapsPath(String mapsPath) {
     this.mapsPath = mapsPath;
   }
 
+  /**
+   * The path of the menus without an ending file separator to the resources
+   * @return The relative path containing File.Separator
+   */
   public String getMenuPath() {
     return menuPath;
   }
 
+  /**
+   * Sets the full relative path of the menu directory
+   * @param menuPath The new directory of the menus
+   */
   public void setMenuPath(String menuPath) {
     this.menuPath = menuPath;
   }
 
+  /**
+   * Gets the width of the size of the map
+   * @return The number of units the map is wide
+   */
   public int getMapWidth() {
     return mapWidth;
   }
 
+  /**
+   * Gets the height of the size of the map
+   * @return The number of units the map is high
+   */
   public int getMapHeight() {
     return mapHeight;
   }
 
+  /**
+   * Gets the number of pixels of the client window
+   * @return The number of pixels the window is wide
+   */
   public int getWindowWidth() {
     return windowWidth;
   }
 
+  /**
+   * Gets the number of pixels of the client window
+   * @return The number of pixels the window is high
+   */
   public int getWindowHeight() {
     return windowHeight;
   }
 
+  /**
+   * The path of the music without an ending file separator
+   * @return The relative path containing File.Separator
+   */
   public String getMusicPath() {
     return musicPath;
   }
 
+  /**
+   * The path of the sound effects without an ending file separator
+   * @return The relative path containing File.Separator
+   */
   public String getSFXPath() {
     return SFXPath;
   }
 
+  /**
+   * The maximum number of players allowed in the game
+   * @return The maximum number of players allowed in the game
+   */
   public int getMaxPlayers() {
     return maxPlayers;
   }
 
+  /**
+   * The global font
+   * @return The global font at the default size
+   */
   public Font getFont() {
     return getFont(defaultFontSize);
   }
 
+  /**
+   * The global font
+   * @param size The size of the font returned
+   * @return The global font at the specified size
+   */
   public Font getFont(int size) {
     try {
       font = Font
@@ -159,34 +222,66 @@ public class Settings {
     this.soundEffectVolume = soundEffectVolume < 0 ? 0 : soundEffectVolume;
   }
 
+  /**
+   * Gets the username of the client's player
+   * @return The name chosen by the client
+   */
   public String getUsername() {
     return username;
   }
 
+  /**
+   * Sets the username of the client's player
+   * @param username The new name chosen by the player
+   */
   public void setUsername(String username) {
     this.username = username;
   }
 
+  /**
+   * The port of the client/server connection
+   * @return The port of the client/server connection
+   */
   public int getPort() {
     return port;
   }
 
+  /**
+   * The port of the clinet/server connection
+   * @param port The port of the client/server conneciton
+   */
   public void setPort(int port) {
     this.port = port;
   }
 
+  /**
+   * The global levelhandler controlling map/level changes
+   * @return Instance of LevelHandler
+   */
   public LevelHandler getLevelHandler() {
     return levelHandler;
   }
 
+  /**
+   *  The global levelhandler controlling map/level changes
+   * @param levelHandler Instance of LevelHandler
+   */
   public void setLevelHandler(LevelHandler levelHandler) {
     this.levelHandler = levelHandler;
   }
 
+  /**
+   * The JavaFX Group which all elements are added to
+   * @return The group root
+   */
   public Group getGameRoot() {
     return gameRoot;
   }
 
+  /**
+   * The JavaFX Group which all elements are added to
+   * @param gameRoot The group root
+   */
   public void setGameRoot(Group gameRoot) {
     this.gameRoot = gameRoot;
   }
