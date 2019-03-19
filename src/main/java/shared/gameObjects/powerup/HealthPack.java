c23fb060d7770811faf70fcb0e6628efee815218
Henry Fung <hlf764@cs.bham.ac.uk>
2019-03-19 15:03:34 +0000

    Powerup to refill health point

---
+++ src/main/java/shared/gameObjects/powerup/HealthPack.java
@@ -0,0 +1,30 @@
+package shared.gameObjects.powerup;
+
+import java.util.UUID;
+import shared.gameObjects.UI.Health;
+import shared.gameObjects.players.Player;
+import shared.util.Path;
+
+public class HealthPack extends Powerup {
+
+  private static String imagePath = "";
+  private static double sizeX = 30, sizeY = 30;
+  private static int VALUE = 30;
+
+  public HealthPack(
+      double x,
+      double y,
+      UUID uuid) {
+    super(x, y, sizeX, sizeY, "HealthPack", uuid);
+  }
+
+  @Override
+  public void apply(Player p) {
+    p.setHealth(p.getHealth() + VALUE);
+  }
+
+  @Override
+  public void initialiseAnimation() {
+    this.animation.supplyAnimation("default", Path.convert(imagePath));
+  }
+}
