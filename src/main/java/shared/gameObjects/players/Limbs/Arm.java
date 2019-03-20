96e19546e0379cf45f3c4bb7bb972033734d5661
Henry Fung <hlf764@cs.bham.ac.uk>
2019-03-20 20:03:44 +0000

    Render differently with/without holding, and smoother

--- src/main/java/shared/gameObjects/players/Limbs/Arm.java
+++ src/main/java/shared/gameObjects/players/Limbs/Arm.java
@@ -7,15 +7,16 @@ import shared.gameObjects.GameObject;
 import shared.gameObjects.Utils.ObjectType;
 import shared.gameObjects.players.Limb;
 import shared.gameObjects.players.Player;
+import shared.gameObjects.weapons.Punch;
 import shared.handlers.levelHandler.LevelHandler;
 import shared.util.maths.Vector2;
 
 public class Arm extends Limb {
 
   private static double AIM_ANGLE_MAX = 110;
-  private static double PI = 3.141592654;
   private static double DEFAULT_ANGLE_LEFT = 6;
   private static double DEFAULT_ANGLE_RIGHT = -9;
+  private static double PI = 3.141592654;
   private double angleRadian;
   private Limb hand;
   private Player playerParent;
@@ -45,28 +46,61 @@ public class Arm extends Limb {
 
   @Override
   public void render() {
-    super.render();
     if (this.hand == null) {
       return;
     }
+
+    imageView.getTransforms().clear();
 
     double xHand = this.getX();
     double yHand = this.getY();
     double xMouse = this.playerParent.mouseX;
     double yMouse = this.playerParent.mouseY;
+    /* 2 situations:
+     *    i. With player have holding and holding not Punch
+     *       - True when the mouse pointer is pointing to the left
+     *       - False otherwise
+     *   ii. With player have holding (a proper gun / melee)
+     *       - True when it is aiming to the left
+     *       - False otherwise
+     */
+    boolean toTheLeft =
+        (playerParent.getHolding() == null || playerParent.getHolding() instanceof Punch) ?
+            playerParent.isPointingLeft()
+            : playerParent.isAimingLeft();
 
-    Vector2 v = new Vector2(xMouse - xHand, yMouse - yHand);
-    float angle = v.angleBetween(Vector2.Zero()) * 180 / 3.141592654f;
-    imageView.getTransforms().clear();
+    Vector2 mouseSubHand = new Vector2(xMouse - xHand, yMouse - yHand);
+    angleRadian = mouseSubHand.angleBetween(Vector2.Zero());
+    double angle = angleRadian * 180 / PI;
+
+    // Change hand when aiming the other way
+    double angleHorizontal; // angle wrt horizontal axis
+    if (toTheLeft) {
+      angleHorizontal = (mouseSubHand.angleBetween(Vector2.Left())) * 180 / PI; // degree
+      if (angleHorizontal > AIM_ANGLE_MAX) {
+        angle = angleHorizontal - 180f;
+      }
+      if (angleHorizontal > 90f) {
+        angle = angleHorizontal * (yMouse > this.getY() ? -1 : 1);
+      }
+    } else { // playerParent pointing Right
+      angleHorizontal = (mouseSubHand.angleBetween(Vector2.Right())) * 180 / PI; // degree
+      if (angleHorizontal > AIM_ANGLE_MAX) {
+        angle = 180f - angleHorizontal;
+      }
+      if (angleHorizontal > 90f) {
+        angle = angleHorizontal * (yMouse > this.getY() ? 1 : -1);
+      }
+    }
 
     if (isLeft) {
-      if (playerParent.isPointingLeft()) {
+      if (toTheLeft) {
         rotate.setAngle(270 - angle);
       } else {
         rotate.setAngle(DEFAULT_ANGLE_LEFT);
       }
-    } else {
-      if (!(playerParent.isPointingLeft())) {
+    } else { // is right arm
+      if (!toTheLeft) {
         rotate.setAngle(270 + angle);
       } else {
         rotate.setAngle(DEFAULT_ANGLE_RIGHT);
@@ -75,6 +109,7 @@ public class Arm extends Limb {
 
     imageView.getTransforms().add(rotate);
 
+    super.render();
   }
 
   @Override
