1c460b8811c9c8e2edf7e1daebc283768561cfc5
Faisal IMH <fxa579@cs.bham.ac.uk>
2019-03-24 19:32:15 +0000

    New character menu and button

--- src/main/java/shared/gameObjects/menu/main/ButtonCharacterCustomisation.java
+++ src/main/java/shared/gameObjects/menu/main/ButtonCharacterCustomisation.java
@@ -1,5 +1,25 @@
 package shared.gameObjects.menu.main;
 
-public class ButtonCharacterCustomisation {
+import java.util.UUID;
+import javafx.scene.input.MouseEvent;
+import shared.gameObjects.Utils.ObjectType;
+import shared.gameObjects.menu.ButtonObject;
+import shared.handlers.levelHandler.Map;
+import shared.util.Path;
 
+public class ButtonCharacterCustomisation extends ButtonObject {
+
+  public ButtonCharacterCustomisation(
+      double x, double y, double sizeX, double sizeY, ObjectType id, UUID objectUUID) {
+    super(x, y, sizeX, sizeY, "USER DATA", id, objectUUID);
+  }
+
+  public void doOnClick(MouseEvent e) {
+    super.doOnClick(e);
+    settings.getLevelHandler().changeMap(
+        new Map(
+            "CHARACTER",
+            Path.convert("src/main/resources/menus/character.map")),
+        false, false);
+  }
 }
