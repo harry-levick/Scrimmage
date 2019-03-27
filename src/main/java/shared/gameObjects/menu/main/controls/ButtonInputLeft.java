93efcc6b68ce7f7fbbd9a9718edef7b5859f9c0d
llc721 <llc721@student.bham.ac.uk>
2019-03-27 00:45:39 +0000

    Change keyboard mapping of LEFT

--- src/main/java/shared/gameObjects/menu/main/controls/ButtonInputLeft.java
+++ src/main/java/shared/gameObjects/menu/main/controls/ButtonInputLeft.java
@@ -1,5 +1,35 @@
 package shared.gameObjects.menu.main.controls;
 
-public class ButtonInputLeft {
+import client.main.Client;
+import client.main.Settings.KEY_CONTROL;
+import java.util.UUID;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import shared.gameObjects.Utils.ObjectType;
+import shared.gameObjects.menu.ButtonObject;
+
+public class ButtonInputLeft extends ButtonObject {
+
+  public ButtonInputLeft(
+      double x, double y, double sizeX, double sizeY, ObjectType id, UUID objectUUID) {
+    super(x, y, sizeX, sizeY, "LEFT: ", id, objectUUID);
+  }
+
+  public void doOnClick(MouseEvent e) {
+    super.doOnClick(e);
+    Client.keyInput.setChangeListening(KEY_CONTROL.LEFT);
+  }
+
+  @Override
+  public void render() {
+    super.render();
+    super.button.setFont(settings.getFont(20));
+    super.setText("LEFT: " + Client.settings.getKeyMap(KEY_CONTROL.LEFT));
+    if (Client.keyInput.getChangeListening() && Client.keyInput.getHotChangeKey() == KEY_CONTROL.LEFT) {
+      super.button.setTextFill(Color.YELLOW);
+    } else {
+      super.button.setTextFill(Color.WHITE);
+    }
+  }
 
 }
