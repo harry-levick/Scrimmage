5c85a7a6e40ec1e5b3eb5a880b384c038b07133e
Henry Fung <hlf764@cs.bham.ac.uk>
2019-03-28 15:47:25 +0000

    Added punch animation

--- src/main/java/shared/gameObjects/players/Limbs/Arm.java
+++ src/main/java/shared/gameObjects/players/Limbs/Arm.java
@@ -10,15 +10,20 @@ import shared.gameObjects.players.Behaviour;
 import shared.gameObjects.players.Limb;
 import shared.gameObjects.players.Player;
 import shared.handlers.levelHandler.LevelHandler;
+import shared.util.maths.Vector2;
 
 /**
  * The Arm object of a player
  */
 public class Arm extends Limb {
 
-  private final int MAX_ANGLE_INDEX = 90;
-  private double[] punchAngles;
-  private int punchAngleIndex;
+  private final float PI = 3.141592654f;
+  /** Default punch timer, which states how long the arm has to raise
+   * while punching before putting it down
+   */
+  private final int DEFAULT_PUNCH_TIMER = 10;
+  /** Current punch timer, 0 will put the arm down */
+  private int punchTimer;
 
   /**
    * Base class used to create an object in game. This is used on both the client and server side to
@@ -32,11 +37,7 @@ public class Arm extends Limb {
     limbMaxHealth = player.getHealth() / 2;
     limbHealth = limbMaxHealth;
 
-    punchAngleIndex = 0;
-    punchAngles = new double[MAX_ANGLE_INDEX];
-    for (int i = 0; i < MAX_ANGLE_INDEX; i++) {
-      punchAngles[i] = (double) i;
-    }
+    punchTimer = DEFAULT_PUNCH_TIMER;
   }
 
   @Override
@@ -45,15 +46,28 @@ public class Arm extends Limb {
     rotate = new Rotate();
     rotate.setPivotX(10);
     rotate.setPivotY(10);
+    punchTimer = DEFAULT_PUNCH_TIMER;
+  }
+
+  @Override
+  public void update() {
+    super.update();
+    --punchTimer;
   }
 
   @Override
   public void render() {
     super.render();
-    if (limbAttached)
+    if (limbAttached) {
       if (player.isPunched()) {
         punchAnimation();
       }
+      if (punchTimer <= 0) {
+        imageView.getTransforms().clear();
+        player.setPunched(false);
+        punchTimer = DEFAULT_PUNCH_TIMER;
+      }
+    }
   }
 
   @Override
@@ -86,7 +100,8 @@ public class Arm extends Limb {
 
   @Override
   public void initialiseAnimation() {
-    this.animation.supplyAnimation("default", "images/player/skin" + settings.getData().getActiveSkin()[2] + "/arm.png");
+    this.animation.supplyAnimation("default",
+        "images/player/skin" + settings.getData().getActiveSkin()[2] + "/arm.png");
   }
 
   @Override
@@ -98,48 +113,44 @@ public class Arm extends Limb {
     // Control to switch the leg animations depending on movement direction.
     boolean control = isLeft;
     int inverse = 1;
-    if(this.behaviour == Behaviour.WALK_LEFT) {
+    if (this.behaviour == Behaviour.WALK_LEFT) {
       control = !control;
       inverse = -1;
     }
-    
-    if(!control) {
-      imageView.setRotate(-130*inverse);
+
+    if (!control) {
+      imageView.setRotate(-130 * inverse);
     }
-    
+
   }
-  
+
   private void walkAnimation() {
-  
+
     int interval = 7;
     int segments = 3;
     int localTime = this.player.getAnimationTimer() % (interval * segments);
-    
+
     boolean control = isLeft;
     int inverse = 1;
-    if(this.behaviour == Behaviour.WALK_LEFT) {
+    if (this.behaviour == Behaviour.WALK_LEFT) {
       control = !control;
       inverse = -1;
     }
-    
+
     // Default is for running left.
-    if(this.behaviour == Behaviour.WALK_LEFT || this.behaviour == Behaviour.WALK_RIGHT) {   
-      
-      if(localTime < interval * 1) {
-        if(control) {
-          imageView.setRotate(60*inverse);
+    if (this.behaviour == Behaviour.WALK_LEFT || this.behaviour == Behaviour.WALK_RIGHT) {
+
+      if (localTime < interval * 1) {
+        if (control) {
+          imageView.setRotate(60 * inverse);
+        } else {
+          imageView.setRotate(-100 * inverse);
         }
-        else {
-          imageView.setRotate(-100*inverse);
-        }
-      }
-      
-      else if(localTime < interval * 2) {
-        if(control) {
-          imageView.setRotate(-40*inverse);
-        }
-        else {
-          imageView.setRotate(-50*inverse);
+      } else if (localTime < interval * 2) {
+        if (control) {
+          imageView.setRotate(-40 * inverse);
+        } else {
+          imageView.setRotate(-50 * inverse);
         }
       }
     }
@@ -147,34 +158,41 @@ public class Arm extends Limb {
 
   @Override
   protected void rotateAnimate() {
-    if(this.behaviour == Behaviour.JUMP || this.player.getJumped()) {
+    if (this.behaviour == Behaviour.JUMP || this.player.getJumped()) {
       jumpAnimation();
-    }
- 
-    else if(this.behaviour == Behaviour.WALK_LEFT || this.behaviour == Behaviour.WALK_RIGHT) {   
+    } else if (this.behaviour == Behaviour.WALK_LEFT || this.behaviour == Behaviour.WALK_RIGHT) {
       walkAnimation();
     }
   }
 
-  protected void punchAnimation() {
-    if (punchAngleIndex >= MAX_ANGLE_INDEX) {
-      player.setPunched(false);
-      punchAngleIndex = 0;
-    }
+  /**
+   * Raise the hand to an angle for DEFAULT_PUNCH_TIMER frames.
+   * Angle is determined by the facing of the player and the mouse position
+   */
+  private void punchAnimation() {
+    double handX, handY;
+    double angle;
+    Vector2 vPunch;
 
-    double angle = punchAngles[punchAngleIndex];
     if (player.isPointingLeft()) {
-      angle = isLeft?
-          (playerFaceLeft? angle-90 : angle)
+      handX = player.getHandLeft().getX();
+      handY = player.getHandLeft().getY();
+      vPunch = new Vector2(player.mouseX - handX, player.mouseY - handY);
+      angle = vPunch.angle();
+      angle = angle * 180 / PI;
+      angle = isLeft ?
+          (playerFaceLeft ? -angle - 90 : angle + 90)
           : 0;
-    }
-    else // is pointing right
-    {
-      angle = isLeft? 0
-          : (!playerFaceLeft? -angle : angle);
+    } else { // is pointing right
+      handX = player.getHandRight().getX();
+      handY = player.getHandRight().getY();
+      vPunch = new Vector2(player.mouseX - handX, player.mouseY - handY);
+      angle = vPunch.angle();
+      angle = angle * 180 / PI;
+      angle = isLeft ? 0
+          : (!playerFaceLeft ? angle - 90 : 90 - angle);
     }
     rotate.setAngle(angle);
     imageView.getTransforms().add(rotate);
-    punchAngleIndex += 8;
   }
 }
