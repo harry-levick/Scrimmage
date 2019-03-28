48739386d41e3a1ed59713047a8534fbbe99aefe
Matt <mjh769@cs.bham.ac.uk>
2019-03-28 22:47:25 +0000

    Added Timer UI

--- src/main/java/client/main/Client.java
+++ src/main/java/client/main/Client.java
@@ -137,7 +137,7 @@ public class Client extends Application {
   private MouseInput mouseInput;
   private Group root;
   private boolean startedGame;
-  private int timeRemaining;
+  private static int timeRemaining;
   private int timeLimit = 1; // Time limit in minutes
   private float maximumStep;
   private long previousTime;
@@ -155,7 +155,7 @@ public class Client extends Application {
    * Sets a new user interface in the uiRoot, rendered by the main game loop depending on use
    */
   public static void setUserInterface() {
-    userInterface = new UI(uiRoot, levelHandler.getClientPlayer(),settings);
+    userInterface = new UI(uiRoot, levelHandler.getClientPlayer(),settings, timeRemaining);
   }
 
   /**
